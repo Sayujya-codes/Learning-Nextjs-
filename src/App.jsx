56b62847a55845df092affb142fcc8395380d5e4
unknown <sayujcodes@gmail.com>
2025-09-15 15:22:33 +0545

    passing para via naviagation

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/contact" element={<ContactPage />}></Route>
-          <Route path="/about" element={<AboutPage />}></Route>
+          <Route path="/about/id:/name" element={<AboutPage />}></Route>
           <Route path="/*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import MenuFile from "../component/MenuFile";
+import { useParams } from "react-router-dom";
 const AboutPage = () => {
+  let { id, name } = useParams();
+
   return (
     <div>
       <MenuFile />
       <h1>This is About Page.</h1>
+      <p>Id: {id}</p>
+      <p>Name: {name}</p>
     </div>
   );
 };
