4a8df5e6f4ae6e45aac56ddbf2e82ef80d241c1d
unknown <sayujcodes@gmail.com>
2025-10-08 10:56:57 +0545

    spread operator

--- src/component/SpreadOperator.jsx
+++ src/component/SpreadOperator.jsx
@@ -5,10 +5,12 @@ const SpreadOperator = () => {
     key1: "Sayujya",
     key2: "Dhungana",
   };
-
   const arrayCopy = { ...array1 };
 
   console.log(arrayCopy);
 };
 
 export default SpreadOperator;
+
+// spread operator is used to make the new copy of the array by spliting up the array or object.
+//
